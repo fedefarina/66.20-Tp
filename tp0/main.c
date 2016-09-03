d6abfd090a491e88824c226e849b3979bfb0b3b4
fjfarina <74123698Fede>
2016-09-03 19:25:15 -0300

    createPGM function

--- tp0/main.c
+++ tp0/main.c
@@ -1,46 +1,22 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
+#include "pgmFactory.h"
 
 int main(int argc, char **argv) {
-    int aflag = 0;
-    int bflag = 0;
-    char *cvalue = NULL;
-    int index;
-    int c;
+    int rows = 3;
+    int cols = 5;
 
-    opterr = 0;
-    while ((c = getopt(argc, argv, "abc:")) != -1)
-        switch (c) {
-            case 'a':
-                aflag = 1;
-                break;
-            case 'b':
-                bflag = 1;
-                break;
-            case 'c':
-                cvalue = optarg;
-                break;
-            case '?':
-                if (optopt == 'c')
-                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-                else if (isprint (optopt))
-                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-                else
-                    fprintf(stderr,
-                            "Unknown option character `\\x%x'.\n",
-                            optopt);
-                return 1;
-            default:
-                abort();
+    int matrix[rows][cols];
+
+
+    int count = 0;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = ++count;
         }
+    }
 
-    printf("aflag = %d, bflag = %d, cvalue = %s\n",
-           aflag, bflag, cvalue);
-
-    for (index = optind; index < argc; index++)
-        printf("Non-option argument %s\n", argv[index]);
+    createPGM(5, 3, 255, "prueba.pgm", matrix);
     return 0;
 }
