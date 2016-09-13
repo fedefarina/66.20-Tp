d54c45855a8dcc14b68d1d2cc6c4b5dd3a03bd28
agscontini <agscontini@gmail.com>
2016-09-13 01:00:36 -0300

    correccion de tests

--- tp0/src/main.c
+++ tp0/src/main.c
@@ -288,8 +288,9 @@ int main(int argc, char **argv) {
         fp = stdout;
     }
 
+
     /* temporal para ver los valores que quedan */
-    printf("resolution width= %d \n", resolutionWidth);
+   /* printf("resolution width= %d \n", resolutionWidth);
     printf("resolution heigth= %d \n", resolutionHeight);
     printf("rectangle width= %f \n", rectangleWidth);
     printf("rectangle heigth= %f \n", rectangleHeight);
@@ -298,7 +299,7 @@ int main(int argc, char **argv) {
     printf("c real= %f \n", cRe);
     printf("c im= %f \n", cIm);
     printf("output= %s \n", output);
-
+*/
 
     int *matrix = doCalculo(centerRe, centerIm, rectangleHeight, rectangleWidth, resolutionWidth, resolutionHeight, cRe,
                             cIm,
